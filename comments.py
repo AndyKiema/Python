14bbe8765f66fed31ae3671c69b46bd40972c532
Andrew Kiema <andrewkiema94@gmail.com>
2025-01-27 10:41:18 +0300

    Add comments

---
+++ comments.py
@@ -0,0 +1,8 @@
+#name:Andrew (You can also use Ctrl+/ to comment a line out)
+#language:python
+# I am learning python
+
+""" I am learning python
+I am learning python
+I am learning python
+I am learning python """
