625b31d301b15547b459cd92194e29c7d5f87367
Andrew Kiema <andrewkiema94@gmail.com>
2025-01-30 10:04:08 +0300

    Datatypes

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+# No need to specify data type like in Java
+
+x=100
+x="Welcome"
+print(x) #Welcome will be printed
+
+#To find out data type of variable
+print(type(x)) #<class 'str'>
+
+y=500
+print(type(y)) #<class 'int'>
+
+z=10.5
+print(type(z)) #<class 'float'> We dont have double in python
+
+p='Yes'
+print(type(p)) #<class 'str'> A string can either be in double or single quotation marks
+
+b=True  #Note: Has to start with uppercase
+print(type(b)) #<class 'bool'> 
