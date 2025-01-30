50acf9911f9cb30d293f3325228460c8ab4cecbd
Andrew Kiema <andrewkiema94@gmail.com>
2025-01-30 13:36:27 +0300

    Arithmetic operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+#Arithmetic operators
+a=100
+b=200
+print(a+b) #300
+sum=a+b
+print(sum)#300
+print(a*b)#20000
+print(b-a)#100
+div=b/a
+print(div)#2
+print(type(div)) #<class 'float'>
+print(a//b)#returns co-efficient a/b=0.5. So it returns 0;
+print(b%a)# returns 0, which is the remainder
+print(a**3)#a to the power of 3 wich is 1,000,000
