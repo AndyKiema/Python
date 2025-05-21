79ebc3cebfd6c856b50e8ef3f65d6cd7a94deab7
Andrew Kiema <andrewkiema94@gmail.com>
2025-05-21 17:50:15 +0300

    Format output

--- concatenation.py
+++ concatenation.py
@@ -1,11 +1,11 @@
-print(10+10) #Valid
+"""print(10+10) #Valid
 print(10.5+20.6) #Valid
 print(10+20.6) #Valid
-print('welcome'+'to'+'python')#Valid
+print('welcome'+'to'+'python')#Valid"""
 
 #True is 1
 #False is 0
-print(True+1) #2
+print(True+2) #3
 print(False+3) #3
 print(True+True)#2
 print(False+False)#0
--- formattingoutput.py
+++ formattingoutput.py
@@ -1,6 +1,7 @@
-"""name='John'
+name='John'
 age=30
-salary=50000.00"""
+intro='My name is'
+salary=50000.00
 
 #Instead of writing three lines of code, just write like this:
 name,age,salary='John',30,50000.00
@@ -10,8 +11,10 @@ print(salary) #50000
 print(name,age,salary) #John 30 50000
 
 print("My name is",name)#My name is John
+print(intro,name)#My name is John
+print("My name is"+name)#My name isJohn
 print("My age is",age)#My age is 30
-print("My salary is",salary,"dollars")#My salary is 50000 dollars
+print("My salary is",salary,"dollars and my name is",name)#My salary is 50000 dollars
 print("My name is %s My age is %d My salary is %g"%(name,age,salary)) # %s=string, %d=digit, %g=decimal number
 print("My name is {} My age is {} My salary is {}".format(name,age,salary)) #My name is John My age is 30 My salary is 50000.0
 print("My name is {} My age is {} My salary is {}".format(salary,age,name)) #My name is 50000.0 My age is 30 My salary is John
