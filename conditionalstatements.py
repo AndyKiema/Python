57c2ef610324b2e814e086cc9113667b06175770
Andrew Kiema <andrewkiema94@gmail.com>
2025-05-28 21:59:22 +0300

    Add conditional statements

---
+++ conditionalstatements.py
@@ -0,0 +1,30 @@
+#Print whether someone is eligible to vote or not(must be 18 years old or older)
+"""age=25
+if age>=18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")"""
+#Print whether a number is even or odd
+"""num=18
+if num%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")"""    
+#Use ternary operators
+"""num=29
+print("Number is even") if num%2==0 else print("Number is odd")"""
+#Ternary operators with multiple statements
+"""num=23
+{print("Hello"),print("python")} if num%2==0 else{print("Hey"),print("Java")}"""
+#Else if statements
+weekno=2
+if weekno==1:
+    print("Monday")
+elif weekno==2:
+    print("Tuesday")
+elif weekno==3:
+    print("Wednesday")
+elif weekno==4:
+    print("Thursday")
+else:
+    print("Invalid")
