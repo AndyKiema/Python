35115a4f181fee2ff32427f9efd001a5d18b5b92
Andrew Kiema <andrewkiema94@gmail.com>
2025-06-07 23:16:40 +0300

    Add for loop

---
+++ forloop.py
@@ -0,0 +1,11 @@
+#for i in range(10):
+   # print(i)
+
+#for i in range(1,10):
+    #print(i)
+##Print even numbers between 1 and 10
+##for i in range(2,11,2):
+    ##print(i)
+## Print even numbers from 10 to 1
+for i in range(10,1,-2):
+  print(i)
