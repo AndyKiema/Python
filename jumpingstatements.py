163870b948e9ed3ecca6ae3d1d6395075bdc023c
Andrew Kiema <andrewkiema94@gmail.com>
2025-06-10 22:25:59 +0300

    Add jumping statements

---
+++ jumpingstatements.py
@@ -0,0 +1,20 @@
+#break
+#for i in range(1,12):
+ #   if i==11:
+  #    break
+   # print(i)      #ANSWER= 1,2,3,4,5,6,7,8,9,10
+
+#for i in range(2,13,2):
+ #   if i==12:
+  #      break
+   # print(i)  #ANSWER= 2,4,6,8,10
+
+#continue
+#for i in range(1,12):
+ #   if i==5:
+  #      continue
+   # print(i)  #ANSWER= 1,2,3,4,6,7,8,9,10,11
+for i in range(2,11,2):
+    if i==8:
+        continue
+    print(i)  #ANSWER= 2,4,6,10
