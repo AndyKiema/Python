0e3d371cb4b24f7a617a2ec817fd569d5a3ef3ae
Andrew Kiema <andrewkiema94@gmail.com>
2025-06-11 16:37:23 +0300

    Strings are immutable

--- stringvariables.py
+++ stringvariables.py
@@ -2,3 +2,11 @@ s='Welcome'
 s="Welcome"
 s=str('Welcome')
 s=str("Welcome")
+"""print(type(s))
+str1='welcome'
+print(id(str1))
+str1='to'
+print(id(str1))
+print(str1)"""
+print(s,"to") #Welcome to
+print(s*4) #WelcomeWelcomeWelcomeWelcome
