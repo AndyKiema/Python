76f4ca21a5e8cad410777347234eebb22999ee01
Andrew Kiema <andrewkiema94@gmail.com>
2025-06-30 11:59:01 +0300

    Add capitalize, title, upper, lower and replace string formulas

---
+++ convertingstrings.py
@@ -0,0 +1,38 @@
+#capitalize()
+#Convert first letter in a string to uppercase
+s='welcome to python'
+s1=s.capitalize()
+print(s) #welcome to python
+print(s1) #Welcome to python
+p='yes sir!'
+p1=p.capitalize()
+print(p) #yes sir!
+print(p1) #Yes sir!
+
+#title()
+#Converts the first letter of each word in a string to uppercase
+s='welcome to python'
+s1=s.title()
+print(s)
+print(s1)
+p='yes sir!'
+p1=p.title()
+print(p) #yes sir!
+print(p1) #Yes Sir!
+
+#lower()
+#Converts all letters in a string to lowercase
+j='YES'
+print(j) #YES
+print(j.lower()) #yes
+
+#upper()
+#Converts all letters in a string to uppercase
+s='welcome to python'
+print(s) #welcome to python
+print(s.upper()) #WELCOME TO PYTHON
+
+#replace()
+#Replaces a substring with another one
+s='welcome to python'
+print(s.replace('to','with'))
