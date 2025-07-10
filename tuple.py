4b9f82eff5fc2e4fbf28a9f8b6a4f3fd41a6ed51
Andrew Kiema <andrewkiema94@gmail.com>
2025-07-10 16:09:10 +0300

    Create a tuple and access its items

---
+++ tuple.py
@@ -0,0 +1,11 @@
+#A tuple is a collection which is ordered and unchangeable
+#Written with round brackets ()
+#Is immutable- values cant be changed, removed or added
+
+#Printing a tuple
+mytuple=('apple','banana','cherry')
+print(mytuple)
+
+#Accessing items from a tuple
+mytuple=('apple','banana','cherry')
+print(mytuple[0])
