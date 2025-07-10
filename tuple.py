5b2855283b5a5a2bbdf930e8e30f03e98a254a95
Andrew Kiema <andrewkiema94@gmail.com>
2025-07-10 16:25:01 +0300

    Print a range

--- tuple.py
+++ tuple.py
@@ -4,8 +4,15 @@
 
 #Printing a tuple
 mytuple=('apple','banana','cherry')
-print(mytuple)
+print(mytuple) #('apple', 'banana', 'cherry')
 
 #Accessing items from a tuple
 mytuple=('apple','banana','cherry')
-print(mytuple[0])
+print(mytuple[0]) #apple
+print(mytuple[-1]) #cherry
+tuple1=()
+print(tuple1) #()
+
+#Printing range 
+mytuple=('apple','banana','cherry')
+print(mytuple[0:2]) #('apple', 'banana')
