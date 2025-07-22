8772ad5b50aee90ebe8b84fcbbad4d12e930a13b
Andrew Kiema <andrewkiema94@gmail.com>
2025-07-22 10:48:43 +0300

    Create a set

---
+++ set.py
@@ -0,0 +1,6 @@
+#A list is a collection that is unordered and unindexed
+#Written in curly brackets({})
+
+#Creating a set
+myset={"apple","banana","cherry"}
+print(myset) #{'cherry', 'apple', 'banana'} NOTICE HOW ORDER HASN'T BEEN MAINTAINED
