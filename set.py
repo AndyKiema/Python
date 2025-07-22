11afa881004be590dba3dcf75049edcefb6c5f92
Andrew Kiema <andrewkiema94@gmail.com>
2025-07-22 11:02:32 +0300

    Check if item is in a set

--- set.py
+++ set.py
@@ -4,3 +4,16 @@
 #Creating a set
 myset={"apple","banana","cherry"}
 print(myset) #{'cherry', 'apple', 'banana'} NOTICE HOW ORDER HASN'T BEEN MAINTAINED
+
+#Accessing items from a set
+#NOT POSSIBLE BECAUSE A SET IS UNINDEXED
+
+#Check if value exists in set
+myset={"apple","banana","cherry"}
+if 'banana' in myset:
+    print('Banana is present')
+else:
+    print('No banana')
+#OR
+print('banana' in myset) #True
+print('mango' in myset) #False
