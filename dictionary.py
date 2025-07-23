fed759a41129a24770c45e1e5dc0dfa8f45b5cb0
Andrew Kiema <andrewkiema94@gmail.com>
2025-07-23 11:51:52 +0300

    Create a dictionary

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+# A collection which is unordered, mutable and indexed
+# Written in curly brackets
+# Has key-value pairs
+mydictionary={101:'x',102:'y',103:'z'}
+print(mydictionary) #{101: 'x', 102: 'y', 103: 'z'}
