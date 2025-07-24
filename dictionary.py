160f7ed20d556ab697e8b0d61f8d57d6006c8cd3
Andrew Kiema <andrewkiema94@gmail.com>
2025-07-24 11:03:16 +0300

    Access items in a dictionary

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,16 @@
 # Has key-value pairs
 mydictionary={101:'x',102:'y',103:'z'}
 print(mydictionary) #{101: 'x', 102: 'y', 103: 'z'}
+
+#Access items to a dictionary
+mydict={
+    'brand':'Buick',
+    'model':'Enclave',
+    'year': 2021
+}
+print(mydict['brand']) #Buick
+print(mydict['model']) #Enclave
+print(mydict['year']) #2021
+#OR
+x=mydict.get('brand') 
+print(x) #Buick
