91f1c78a481db6363faef5e8c0119c240b38d09d
Andrew Kiema <andrewkiema94@gmail.com>
2025-08-05 17:14:58 +0300

    Identify variables if they have same name

--- OOP.py
+++ OOP.py
@@ -27,10 +27,33 @@ mc2.m1() #This is an instance method
 mc2.m2(100) #100
 
 #Class variables
-class newClass:
+"""class newClass:
   a,b=10,20 #a and b are class variables
   def find(self):
-    print(self.a,self.b) #This is to access the class variables
+    print(self.a,b) #This is to access the class variables
 nc=newClass()
 
-nc.find() #10 20
+nc.find() #10 20"""
+"""x,y=10,20 #Global variable
+class newClass:
+  i,j=5,55 #Class variable
+  def pr(self):
+    p,q=6,7 #Local variable
+    print(x,y) #10 20
+    print(self.i,self.j) #5 55
+    print(p,q) #6 7
+
+nc=newClass()
+nc.pr()"""
+
+x,y=10,20 #Global variable
+class newClass:
+  x,y=5,55 #Class variable
+  def pr(self):
+    x,y=6,7 #Local variable
+    print(x,y) #6 7 local variable
+    print(self.x,self.y) #5 55 class variable
+    print(globals()['x'],globals()['y']) #10 20 global variable
+
+nc=newClass()
+nc.pr()
