2b8fb763fecf15e29a16840c857db443ba772849
Andrew Kiema <andrewkiema94@gmail.com>
2025-08-14 17:25:40 +0300

    Access sub-package

---
+++ client2.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('C:/Users/Administrator/Desktop/python/package1')
+sys.path.append('C:/Users/Administrator/Desktop/python/package1/package2')
+"""import module1
+import module2
+module1.display() #This is display function from module 1
+module2.show() #This is show function"""
+#OR
+from module1 import display
+from module2 import show
+display() #This is display function from module 1
+show() #This is show function
